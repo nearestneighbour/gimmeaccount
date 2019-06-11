7e31cdd57aa2ecdd7f8681b659f5d1c3a43671f8
nearestneighbour <tido_berg@hotmail.com>
2019-06-11 22:43:01 +0200

    file/dir reorganization

--- compile.sh
+++ compile.sh
@@ -1,4 +1,2 @@
 # Because fuck eosio-cpp
-eosio-cpp -I=. -contract=gimmeaccount -o=gimme.wasm gimmeaccount.cpp
-mv gimme.wasm gimmeaccount.wasm
-mv gimme.abi gimmeaccount.abi
+eosio-cpp -contract=gimmeaccount -o=gimmeaccount/gimmeaccount.wasm src/gimmeaccount.cpp
