6ea6618d08713addcc7c247a3a2cf9a8a32c7115
nearestneighbour <tido_berg@hotmail.com>
2019-06-13 20:36:43 +0200

    code cleanup

--- src/gimmeaccount.cpp
+++ src/gimmeaccount.cpp
@@ -1,46 +1,38 @@
 #include "gimmeaccount.hpp"
+#include "base58.hpp"
 
 using namespace eosio;
 
-[[eosio::action]]
-void gimmeaccount::testdecoding(std::string pk_str, public_key pk_obj) {
-    public_key pubkey = decode_pubkey(pk_str);
-    if (pubkey == pk_obj)
-        print_f("Public keys are the same.");
-    else
-        print_f("Public keys are not the same (or decoding doesn't work).");
-}
-
+/*
+    Use the funds from an incoming EOS transaction to create an account
+    based on information provided in the memo of the transaction.
+*/
 [[eosio::on_notify("eosio.token::transfer")]]
 void gimmeaccount::on_transfer(name from, name to, asset quantity, std::string memo) {
-    // Only act on incoming transactions, ignore outgoing
+    // Only act on incoming EOS transactions, ignore everything else
     if (to != get_self()) {
         return;
     }
-    // Assert that token contract == eosio.token and that quantity > xxx
     check(get_first_receiver()=="eosio.token"_n, "Only EOS transfers allowed");
 
-    //Check tx details (memo should be [(12 characters),(53 charaters)])
-    check(memo.length() == 66 && memo[12] == ',', "Memo format is wrong");
+    // Check tx memo - should be "<12 characters>,<53 charaters>"
+    check(memo.length() == 66 && memo[12] == ',',
+        "Memo should be <account name (12chr)>,<public key (53chr)>");
+    // Check if funds are enough to create new account
     asset ramcost = get_ram_price();
-    asset netcost = asset(1000, EOS_sym);
+    asset netcost = asset(1000, EOS_sym); // Use 0.1 EOS for NET/CPU
     asset cpucost = asset(1000, EOS_sym);
     check(quantity >= (ramcost + netcost + cpucost), "Amount of EOS too low");
     asset remaining = quantity - (ramcost+netcost+cpucost);
 
-    //Check parameters
+    // Check account name and public key
     name accname = name(memo.substr(0,12));
-    //check(!is_account(accname), "Account already exists");
+    check(!is_account(accname), "Account already exists");
     std::string pk_str = memo.substr(13);
     public_key pk_obj = decode_pubkey(pk_str);
 
     // Create account, buy RAM, delegate BW
-    authority auth = authority{
-        .threshold = 1,
-        .keys = {key_weight{pk_obj,1}},
-        .accounts = {},
-        .waits = {}
-    };
+    auth.keys = {key_weight{pk_obj,1}};
     action(
         permission_level(get_self(), "active"_n),
         "eosio"_n,
@@ -67,18 +59,32 @@ void gimmeaccount::on_transfer(name from, name to, asset quantity, std::string m
     ).send();
 }
 
+/*
+    Test public key decoding. Enter the same 53 character public key string for
+    each parameter, if decoding works it should recognize that they are the
+    same.
+*/
+[[eosio::action]]
+void gimmeaccount::testdecoding(std::string pk_str, public_key pk_obj) {
+    public_key pubkey = decode_pubkey(pk_str);
+    if (pubkey == pk_obj)
+        print_f("Public keys are the same.");
+    else
+        print_f("Public keys are not the same (or decoding doesn't work).");
+}
+
 //Inspired by http://knowledge.cryptokylin.io/topics/115
 public_key gimmeaccount::decode_pubkey(std::string pk_str) {
-    //Remove EOS prefix and check pubkey length
+    // Remove EOS prefix and check pubkey length
     pk_str = pk_str.substr(3);
     check(pk_str.length() == 50, "Public key string wrong length");
 
-    //Decode pubkey
+    // Decode public key
     const std::array<unsigned char, 37> decoded = DecodeBase58(pk_str);
     std::array<char, 33> pk_data;
     std::copy_n(decoded.begin(), 33, pk_data.begin());
 
-    //Evaluate checksum
+    // Evaluate checksum
     checksum160 pk_cs = ripemd160(reinterpret_cast<char*>(pk_data.data()), 33);
     std::array<uint8_t, 20> cs_data = pk_cs.extract_as_byte_array();
     check(std::memcmp(cs_data.begin(), &decoded.end()[-4], 4) == 0, "Public key checksum mismatch");
@@ -88,10 +94,10 @@ public_key gimmeaccount::decode_pubkey(std::string pk_str) {
 
 // Return cost of 4 KiB of RAM
 asset gimmeaccount::get_ram_price() {
-    multi_index<"rammarket"_n, rammarket> rp(name("eosio"), name("eosio").value);
+    multi_index<"rammarket"_n, rammarket> rp("eosio"_n, "eosio"_n.value);
     auto it = rp.find(symbol("RAMCORE", 4).raw());
     check(it != rp.end(), "Could not get RAM price");
     uint64_t base = it->base.balance.amount;
     uint64_t quote = it->quote.balance.amount;
-    return asset(4096*(double)quote/base, EOS_sym);
+    return asset((double)4096*quote/base, EOS_sym);
 }
--- src/gimmeaccount.hpp
+++ src/gimmeaccount.hpp
@@ -1,8 +1,6 @@
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
 #include <eosio/crypto.hpp>
-#include <array>
-#include "base58.hpp"
 
 using namespace eosio;
 
@@ -11,52 +9,36 @@ class [[eosio::contract("gimmeaccount")]] gimmeaccount : public contract {
         gimmeaccount(name receiver, name code, datastream<const char*> ds):
             contract(receiver, code, ds) {}
 
-        [[eosio::action]]
-        void testdecoding(std::string pkstr, public_key pkobj);
-
         [[eosio::on_notify("eosio.token::transfer")]]
         void on_transfer(name from, name to, asset quantity, std::string memo);
+
+        [[eosio::action]]
+        void testdecoding(std::string pkstr, public_key pkobj);
 
     private:
         public_key decode_pubkey(std::string pk_str);
         asset get_ram_price();
 
-        //Structs for newaccount action
-        struct key_weight {
-            public_key key;
-            uint16_t weight;
-        };
-        struct perm_weight {
-            permission_level permission;
-            uint16_t weight;
-        };
-        struct wait_weight {
-            uint32_t wait_sec;
-            uint16_t weight;
-        };
+        // Structs for newaccount action
+        struct key_weight { public_key key; uint16_t weight; };
+        struct perm_weight { permission_level perm; uint16_t weight; };
+        struct wait_weight { uint32_t wait_sec; uint16_t weight; };
         struct authority {
             uint32_t threshold;
             std::vector<key_weight> keys;
             std::vector<perm_weight> accounts;
             std::vector<wait_weight> waits;
-        };
-        struct newaccount { // Newaccount parameters
-            name creator;
-            name accname;
-            authority owner;
-            authority active;
-        };
+        } auth{ 1, {}, {}, {} };
 
-        //Structs for RAM table/action
-        struct rammarket { //Inspired by https://github.com/GetScatter/CreateBridge/
+        // Structs for RAM table
+        // Inspired by https://github.com/GetScatter/CreateBridge/
+        struct connector { asset balance; double weight = .5; };
+        struct rammarket {
             asset supply;
-            struct connector {
-                asset balance;
-                double weight = .5;
-            };
             connector base;
             connector quote;
             uint64_t primary_key()const {return supply.symbol.raw();}
         };
+
         symbol EOS_sym = symbol("EOS", 4);
 };
